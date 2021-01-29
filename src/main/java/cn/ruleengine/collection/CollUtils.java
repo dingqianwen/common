7bed8c5653bdba0d7b04e45af38c105bacf5018d
dqw <761945125@qq.com>
2021-01-29 20:09:33 +0800

    开发点工具

--- src/main/java/cn/ruleengine/collection/CollUtils.java
+++ src/main/java/cn/ruleengine/collection/CollUtils.java
@@ -4,6 +4,10 @@ package cn.ruleengine.collection;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * 〈一句话功能简述〉<br>
@@ -37,6 +41,11 @@ public class CollUtils {
             arrayList.add(subList);
         }
         return arrayList;
+    }
+
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 
 }
---
+++ src/test/java/cn/ruleengine/collection/CollUtilsTest.java
@@ -0,0 +1,35 @@
+package cn.ruleengine.collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author dingqianwen
+ * @date 2021/1/29
+ * @since 1.0.0
+ */
+public class CollUtilsTest {
+
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(1, "1"));
+        users.add(new User(1, "2"));
+        List<User> collect = users.stream().filter(CollUtils.distinctByKey(User::getName)).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class User {
+        private Integer id;
+        private String name;
+    }
+
+}
