8a7e447d7e0df8e6f3bddc08ca6a3fef5bf13690
dqw <761945125@qq.com>
2021-02-18 12:07:48 +0800

    add

---
+++ src/main/java/cn/ruleengine/common/vo/PageResponse.java
@@ -0,0 +1,32 @@
+package cn.ruleengine.common.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 丁乾文
+ * @create 2019/9/30
+ * @since 1.0.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ApiModel
+public class PageResponse extends PageBase {
+
+    private static final long serialVersionUID = -7378163356547441491L;
+
+    @ApiModelProperty("总记录数")
+    private Long total;
+
+    public PageResponse(long pageIndex, long pageSize, long total) {
+        super.setPageIndex(pageIndex);
+        super.setPageSize(pageSize);
+        this.total = total;
+    }
+
+}
---
+++ src/main/java/cn/ruleengine/common/vo/PlainResult.java
@@ -0,0 +1,26 @@
+package cn.ruleengine.common.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 丁乾文
+ * @create 2019/9/30
+ * @since 1.0.0
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ApiModel("返回普通数据")
+public class PlainResult<T> extends BaseResult{
+
+    private static final long serialVersionUID = 8794822903345524683L;
+
+    @ApiModelProperty("数据")
+    private T data;
+
+}
